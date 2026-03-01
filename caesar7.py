a39dd83c0884c18fd2eacb3a9e69b3e36eb82b2b
Lishavadivelmurgan <LISHAMURGAN2106@GMAIL.COM>
2026-03-01 21:52:30 +0530

    Update caesar7.py

--- caesar7.py
+++ caesar7.py
@@ -1,3 +1,7 @@
+Write a Python program to implement Caesar Cipher encryption with a shift value of 3. The program should take the plaintext as
+input from the user and display the corresponding encrypted text as output.
+
+
 def caesar_cipher_encrypt(plain_text, shift):
     encrypted_text = ""
     
@@ -18,4 +22,5 @@ plain_text = input("Enter the text to encrypt: ")
 shift = int(input("Enter the shift value: "))
 
 encrypted_text = caesar_cipher_encrypt(plain_text, shift)
+
 print("Encrypted text:", encrypted_text)
