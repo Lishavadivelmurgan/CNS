b402bf01cd6f44904ee9f18f03ba72f9ac79c61d
Lishavadivelmurgan <LISHAMURGAN2106@GMAIL.COM>
2026-03-01 21:54:33 +0530

    Update fakewebsite11.py

--- fakewebsite11.py
+++ fakewebsite11.py
@@ -1,3 +1,4 @@
+Fake Website Detection
 import hashlib
 trusted_dns_records={
     "Google.com":hashlib.sha256("Google.com".encode()).hexdigest(),
@@ -17,3 +18,4 @@ if user_domain in trusted_dns_records:
 else:
     print("\n Fake website Detected!")
     print("Domain not found in trusted DNS records.")
+
